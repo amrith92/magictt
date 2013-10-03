96e53c21c7fa27cfd9f0e7ba5943117bcb95f557
Amrith Nayak <amrith92@gmail.com>
2013-10-03 11:10:49 +0530

    Tied up loose ends

--- Library/Controller/IndexController.php
+++ Library/Controller/IndexController.php
@@ -8,14 +8,8 @@ class IndexController extends AbstractController {
 		parent::__construct();
 	}
 	
-	public function index(array $forms_arr = array()) {	
-		if (isset($forms_arr['registration'])) {
-			$forms['registration'] = $forms_arr['registration'];
-		} 
-		if (isset($forms_arr['login'])) {
-			$forms['login'] = $forms_arr['login'];
-		} 
-
-		$this->renderView('index.php',\compact('forms')));
+	public function index() {
+		$this->renderView('index.php');
 	}
 }
+
--- index.php
+++ index.php
@@ -1,2 +1,49 @@
 <?php
-echo '<pre>' . var_export($_SERVER, true) . '</pre>';
+
+require 'Library/Autoload/SplClassLoader.php';
+
+$loader = new SplClassLoader('Library', __DIR__);
+
+$loader->register();
+
+// Config
+define ('ROOT', __DIR__);
+define ('BASE_PATH', 'http://' . $_SERVER['HTTP_HOST']);
+define ('UI_PATH', 'http://' . $_SERVER['HTTP_HOST'] . '/ui');
+
+$controller = null;
+$action = null;
+$params = array();
+
+if(isset($_GET['url'])) {
+	$urlArray = array();
+	$urlArray = explode("/", $_GET['url']);
+	$controller = $urlArray[0];
+	array_shift($urlArray);
+	
+	if (isset($urlArray[0])) {
+		$action = $urlArray[0];
+		array_shift($urlArray);
+	} else {
+		$action = 'index'; // Default Action
+	}
+	
+	$params = $urlArray;
+}
+
+use Library\Controller\FrontController;
+
+try {
+	$frontController = new FrontController(array(
+		'path' => ROOT . DIRECTORY_SEPARATOR . 'Controller',
+		'controller' => $controller,
+		'action' => $action,
+		'params' => $params
+	));
+
+	$frontController->run();
+} catch(\Exception $e) {
+	echo '<strong>THIS</strong> went wrong -&gt; <code>' . $e->getMessage() . '</code>';
+	echo '<pre>' . $e->getTraceAsString() . '</pre>';
+}
+
